17f823a8ddc282077bfea8647d512586f7fb4d04
Kaushal Patel <kaushalpatel@Kaushals-iMac.fios-router.home>
2022-04-28 21:41:10 -0400

    w

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('applicationSelect.html')
+
+@app.route('/hrLogin')
+def hrLogin():
+    return render_template('hrLogin.html')
+
+@app.route('/projLogin')
+def projLogin():
+    return render_template('projLogin.html')
 
 @app.route('/logout')
 def logout():
-    return render_template('login.html')
+    return render_template('applicationSelect.html')
 
 @app.route('/checkLogin', methods=['POST', 'GET'])
 def login():
@@ -34,12 +42,12 @@ def login():
         values = [employeeId, epassword]
         cursor.execute(stat, tuple(values))
         data = cursor.fetchone()
-      
+
         if data is None:
             # if a user enters wrong password return it to the login page
-            return render_template('login.html')
+            return render_template('/hrLogin.html')
         else:
-            return render_template('welcome.html')
+            return render_template('welcomeHR.html')
 
 
 # Press the green button in the gutter to run the script.
